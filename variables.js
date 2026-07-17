6ff5619841099817a790730b05b4fbc0e63d084b
Laksh9341 <laksh9341@gmail.com>
2026-07-17 21:01:13 +0530

    task 3

--- variables.js
+++ variables.js
@@ -5,3 +5,6 @@ console.log(studentName); // printing the value
 //Daily Task 2
 let rollNumber = 12345; // Declared the variable and assigned the roll number
 console.log(rollNumber); // printing the value
+//Daily Task 3
+let isPassed = true; // Declared the variable and assigned the value
+console.log(isPassed); // printing the value
--- variables.ts
+++ variables.ts
@@ -6,3 +6,9 @@ console.log(studentName) // printing the value
 //Daily Task 2
 let rollNumber: number = 12345  // Declared the variable and assigned the roll number
 console.log(rollNumber) // printing the value
+
+
+//Daily Task 3
+let isPassed: boolean = true // Declared the variable and assigned the value
+console.log(isPassed) // printing the value
+
